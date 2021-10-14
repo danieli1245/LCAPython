c13b6fcdc5c73f11a45bb831e48a60ecff16a2cc
danieli1245 <ilyind@tcd.ie>
2021-10-14 22:33:24 +0100

    Initial commit

---
+++ LCATest.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from LCA import Node, findLCA, findPath
+
+
+class LCATest(TestCase):
+
+    def test_EmptyRoot(self):
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        self.assertEqual(findLCA(root, 8, 4), -1, "LCA of 8 and 4 is ")
+
+
+    def test_LCATest(self):
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        root.right.left = Node(6)
+        root.right.right = Node(7)
+        root.left.right.right = Node(8)
+        self.assertEqual(findLCA(root, 8, 4), 2, "LCA of 8 and 4 is ")
+        self.assertEqual(findLCA(root, 0, 1), -1, "LCA of 0 and 1 is ")
+
