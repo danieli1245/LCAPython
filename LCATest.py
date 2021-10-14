010413c9bd66f4ee57da7ae1b07ccc800cd97855
danieli1245 <ilyind@tcd.ie>
2021-10-14 22:55:06 +0100

    added more tests

--- LCATest.py
+++ LCATest.py
@@ -23,3 +23,15 @@ class LCATest(TestCase):
         self.assertEqual(findLCA(root, 8, 4), 2, "LCA of 8 and 4 is ")
         self.assertEqual(findLCA(root, 0, 1), -1, "LCA of 0 and 1 is ")
 
+    def test_LLLCATest(self):
+        root = Node(1)
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(4)
+        root.left.right = Node(5)
+        root.left.left.right = Node(6)
+        root.left.left.left = Node(7)
+        root.left.right.right = Node(8)
+        self.assertEqual(findLCA(root, 8, 1), 1, "LCA of 8 and 1 is ")
+        self.assertEqual(findLCA(root, 7, 3), 1, "LCA of 8 and 1 is ")
+        self.assertEqual(findLCA(root, 0, 1), -1, "LCA of 0 and 1 is ")
